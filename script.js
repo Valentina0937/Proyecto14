55e157e5656060c4b2df798cff52242bf88a731a
Valentina0937 <angela.saavedra16@soy.sena.edu.co>
2023-11-29 15:34:55 -0500

    commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const inputTarjeta = document.getElementById("inputTarjeta");
+const inputFecha = document.getElementById("inputFecha");
+const inputCVV = document.getElementById("inputCVV");
+
+const mascaraNumero = "####-####-####-####";
+const mascaraFecha = "##/##";
+const mascaraCVV = "###";
+
+let TarjetaNumero = [];
+let FechaaNumero = [];
+let cvvNumero = [];
+
+//Keydown Escucha las teclas
+inputTarjeta.addEventListener("keydown",(e)=>{
+    if(e.key == "Tab"){
+        return;
+    }
+    e.preventDefault();
+    ingresoDatos(mascaraNumero, e.key, TarjetaNumero);
+    inputTarjeta.value = TarjetaNumero.join("");
+});
+
+inputFecha.addEventListener("keydown", (e) =>{
+    if (e.key == "Tab"){
+        return;
+    }
+    e.preventDefault();
+    ingresoDatos(mascaraFecha, e.key, FechaaNumero);
+    inputFecha.value = FechaaNumero.join("");
+});
+
+inputCVV.addEventListener("keydown", (e)=>{
+    if (e.key === "Tab"){
+        return;
+    }
+    e.preventDefault();
+    ingresoDatos(mascaraCVV, e.key, cvvNumero);
+    inputCVV.value = cvvNumero.join("");
+});
+
+function ingresoDatos(mascara, key, arreglo){
+    let numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    if (key === "Backsapce" && arreglo.length > 0){
+        //El pop elimina el ultimo elemento del arreglo
+        arreglo.pop();
+        return;
+    }
+    if(numeros.includes(key) && arreglo.length + 1 <= mascara.length){
+        if(mascara[arreglo.length] === "-" || mascara[arreglo.length] === "/"){
+            arreglo.push(mascara[arreglo.length], key);
+        }else{
+            arreglo.push(key);
+        }
+    }
+};
